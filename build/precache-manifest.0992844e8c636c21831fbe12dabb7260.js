62a163421f81edbbc8f64fd2c1683f5e81657e40
MARY SANNI <marysanni@MARYs-MacBook-Pro.local>
2023-03-27 15:47:43 -0500

    commit

---
+++ build/precache-manifest.0992844e8c636c21831fbe12dabb7260.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3b0f1968bceedae62ca80b8e43cb088",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f8d09edf9b17ada62216",
+    "url": "/static/css/main.bb1cd6a4.chunk.css"
+  },
+  {
+    "revision": "86beb5118d7b4ae9f4b2",
+    "url": "/static/js/2.54b5cbf2.chunk.js"
+  },
+  {
+    "revision": "c21d579849576bf6a47550572ff70f62",
+    "url": "/static/js/2.54b5cbf2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8d09edf9b17ada62216",
+    "url": "/static/js/main.9188ed31.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.9188ed31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b6b46ef1e17d32e6df4",
+    "url": "/static/js/runtime-main.bce919bb.js"
+  }
+]);
