0a193abe0b9c20845bcf47fae9aff9ce033a3532
MARY SANNI <marysanni@MARYs-MacBook-Pro.local>
2023-03-27 21:18:49 -0500

    commit

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -8,11 +8,5 @@ module.exports = function(app) {
 
        }) 
     );
-    app.use(
-        '/api2',
-        createProxyMiddleware({
-            target: 'https://aa-52yv.onrender.com',
-            changeOrigin: true,
-        })
-    );
+    
 };
